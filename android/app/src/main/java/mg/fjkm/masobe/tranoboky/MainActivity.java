94c6811ed1c25d360e2dbd3fb4a8832aebad06af
jennypatrickn <njennypatrick@gmail.com>
2026-07-19 00:14:21 +0300

    first commit

---
+++ android/app/src/main/java/mg/fjkm/masobe/tranoboky/MainActivity.java
@@ -0,0 +1,6 @@
+package mg.fjkm.masobe.tranoboky;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mg.fjkm.masobe.tranoboky',
+  appName: 'Trano Boky',
+  webDir: 'www'
+};
+
+export default config;
